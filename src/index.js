10ab6f5cbfcf75e6796d39e7c857d33e6b69ebe5
David <davidpanal@gmail.com>
2021-07-14 20:34:03 +0200

    Add rest of the sheets

--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ exports.__esModule = true;
 var fs = require("fs");
 var path = require("path");
 var xlsx = require("xlsx");
-var moviesPath = path.resolve('F:/Peliculas');
-var movies = fs.readdirSync(moviesPath);
-console.log(movies);
-var excelFile = path.resolve('C:/Users/weyla/Downloads/movies.xlsx');
-var workbook = xlsx.readFile(excelFile);
-var first_sheet_name = workbook.SheetNames[0];
-var worksheet = workbook.Sheets[first_sheet_name];
-movies.forEach(function (element, index) {
-    xlsx.utils.sheet_add_aoa(worksheet, [[element]], { origin: "A" + index });
-});
-xlsx.writeFile(workbook, "C:/Users/weyla/Downloads/movies.xlsx");
+var moviesPath = path.resolve('D:/Peliculas');
+try {
+    // Read movie names on directory
+    var moviesInDirectory = fs.readdirSync(moviesPath);
+    //Read Excel and write movies on it
+    var excelFile = path.resolve('C:/Users/Aorus/OneDrive/Peliculas.xlsx');
+    var workbook = xlsx.readFile(excelFile);
+    var all_movies_sheet = workbook.SheetNames[0];
+    var lacie_sheet = workbook.SheetNames[1];
+    var wd_label_sheet = workbook.SheetNames[2];
+    var wd_no_label_sheet = workbook.SheetNames[3];
+    var worksheet_1 = workbook.Sheets[wd_label_sheet];
+    moviesInDirectory.forEach(function (element, index) {
+        xlsx.utils.sheet_add_aoa(worksheet_1, [[element]], { origin: "A" + (index + 1) });
+    });
+    xlsx.writeFile(workbook, "C:/Users/Aorus/OneDrive/Peliculas.xlsx");
+}
+catch (error) {
+    console.log(error);
+}
--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,28 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as xlsx from 'xlsx';
 
-const moviesPath = path.resolve('F:/Peliculas');
+const moviesPath = path.resolve('D:/Peliculas');
 
 try {
     // Read movie names on directory
     const moviesInDirectory = fs.readdirSync(moviesPath);
 
     //Read Excel and write movies on it
-    const excelFile = path.resolve('C:/Users/weyla/Downloads/movies.xlsx');
+    const excelFile = path.resolve('C:/Users/Aorus/OneDrive/Peliculas.xlsx');
     let workbook = xlsx.readFile(excelFile);
 
-    let first_sheet_name = workbook.SheetNames[0];
+    let all_movies_sheet = workbook.SheetNames[0];
+    let lacie_sheet = workbook.SheetNames[1];
+    let wd_label_sheet = workbook.SheetNames[2];
+    let wd_no_label_sheet = workbook.SheetNames[3];
 
-    let worksheet = workbook.Sheets[first_sheet_name];
+    let worksheet = workbook.Sheets[wd_label_sheet];
 
     moviesInDirectory.forEach((element: any, index: any) => {
-        xlsx.utils.sheet_add_aoa(worksheet, [[element]], {origin: `A${index}`});
+        xlsx.utils.sheet_add_aoa(worksheet, [[element]], {origin: `A${index + 1}`});
     });
 
-    xlsx.writeFile(workbook, "C:/Users/weyla/Downloads/movies.xlsx");
+    xlsx.writeFile(workbook, "C:/Users/Aorus/OneDrive/Peliculas.xlsx");
 } catch (error) {
     console.log(error);
 }
