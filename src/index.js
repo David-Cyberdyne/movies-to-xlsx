dcc67c2727bfc1b20d5564752a44162b7815dbde
David <davidpanal@gmail.com>
2021-07-26 10:53:53 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ var moviesPath = path.resolve('D:/Peliculas');
 try {
     // Read movie names on directory
     var moviesInDirectory = fs.readdirSync(moviesPath);
-    //Read Excel and write movies on it
     var excelFile = path.resolve('C:/Users/Aorus/OneDrive/Peliculas.xlsx');
+    //Read Excel and write movies on it
     var workbook = xlsx.readFile(excelFile);
     var all_movies_sheet = workbook.SheetNames[0];
     var lacie_sheet = workbook.SheetNames[1];
     var wd_label_sheet = workbook.SheetNames[2];
     var wd_no_label_sheet = workbook.SheetNames[3];
-    var worksheet_1 = workbook.Sheets[wd_label_sheet];
+    var worksheet_1 = workbook.Sheets[wd_no_label_sheet];
     moviesInDirectory.forEach(function (element, index) {
         xlsx.utils.sheet_add_aoa(worksheet_1, [[element]], { origin: "A" + (index + 1) });
     });
-    xlsx.writeFile(workbook, "C:/Users/Aorus/OneDrive/Peliculas.xlsx");
+    xlsx.writeFile(workbook, excelFile);
 }
 catch (error) {
     console.log(error);
--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ const moviesPath = path.resolve('D:/Peliculas');
 try {
     // Read movie names on directory
     const moviesInDirectory = fs.readdirSync(moviesPath);
+    const excelFile = path.resolve('C:/Users/Aorus/OneDrive/Peliculas.xlsx');
 
     //Read Excel and write movies on it
-    const excelFile = path.resolve('C:/Users/Aorus/OneDrive/Peliculas.xlsx');
     let workbook = xlsx.readFile(excelFile);
 
     let all_movies_sheet = workbook.SheetNames[0];
@@ -17,13 +17,13 @@ try {
     let wd_label_sheet = workbook.SheetNames[2];
     let wd_no_label_sheet = workbook.SheetNames[3];
 
-    let worksheet = workbook.Sheets[wd_label_sheet];
+    let worksheet = workbook.Sheets[wd_no_label_sheet];
 
     moviesInDirectory.forEach((element: any, index: any) => {
         xlsx.utils.sheet_add_aoa(worksheet, [[element]], {origin: `A${index + 1}`});
     });
 
-    xlsx.writeFile(workbook, "C:/Users/Aorus/OneDrive/Peliculas.xlsx");
+    xlsx.writeFile(workbook, excelFile);
 } catch (error) {
     console.log(error);
 }
